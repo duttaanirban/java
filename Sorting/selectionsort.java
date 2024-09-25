91603c810edb463316519cf564bb657f4394fbd7
Anirban Dutta <duttaanirban@gmail.com>
2024-09-25 20:39:11 +0530

    selectionsort

---
+++ Sorting/selectionsort.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class selectionsort {
+    public static void main(String[] args) {
+        int[] arr = {-12, 8, 3, 0, 3, 7, 67, 100, 9};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+}
