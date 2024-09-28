2122e7a41ba242eab5fd272c519f996cb3b761d6
Anirban Dutta <duttaanirban@gmail.com>
2024-09-28 22:13:48 +0530

    new2

---
+++ Sorting/AllDuplicateNumbers.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllDuplicateNumbers {
+    public List<Integer> findDuplicates(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            // Make sure to only swap when nums[i] is within bounds and not already in the correct position
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index+1) {
+                ans.add(nums[index]);
+            }
+        }
+        return ans;
+    }
+
+    static void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+
+    public static void main(String[] args) {
+        AllDuplicateNumbers AllDuplicateNumbers = new AllDuplicateNumbers();
+        int[] nums = {4,3,2,7,8,2,3,1};
+        List<Integer> allduplicates = AllDuplicateNumbers.findDuplicates(nums);
+        System.out.println(allduplicates);
+    }
+    
+}
