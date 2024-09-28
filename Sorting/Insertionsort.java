c4ce8504f5642a287567a3f5099f45980232cc17
Anirban Dutta <duttaanirban@gmail.com>
2024-09-28 17:16:49 +0530

    insertion

---
+++ Sorting/Insertionsort.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class Insertionsort {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 4, 1, 2};
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j > 0; j--) {
+                if (arr[j] < arr[j-1]) {
+                    swap(arr, j, j-1);
+                }
+                else {
+                    break;
+                }
+            }
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+    
+}
