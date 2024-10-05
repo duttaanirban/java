c892515cdf676a12138b7dfbaacc7b41dca3695e
Anirban Dutta <duttaanirbanad@gmail.com>
2024-10-05 16:01:17 +0530

    recursion

---
+++ recursion/binarySearch.java
@@ -0,0 +1,7 @@
+public class binarySearch {
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
---
+++ recursion/fibonacci.java
@@ -0,0 +1,15 @@
+public class fibonacci {
+    public static void main(String[] args) {
+        int ans = fibo(6);
+        System.out.println(ans);
+    }
+
+    static int fibo(int n) {
+        //base condition
+        if (n<2) {
+            return n;
+        }        
+        return fibo(n-1) + fibo(n-2);
+    }
+    
+}
