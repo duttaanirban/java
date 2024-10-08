dd47fd3090f99efff00b984905ae32d6de0d9af1
Anirban Dutta <duttaanirbanad@gmail.com>
2024-10-09 00:57:18 +0530

    prime

---
+++ functions/PrimeCheck.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static boolean isPrime(int n) {
+        // Edge cases
+        if (n <= 1) return false;
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        // Check from 5 to sqrt(n), skipping even numbers
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (isPrime(num)) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+
+        sc.close();
+    }
+}
