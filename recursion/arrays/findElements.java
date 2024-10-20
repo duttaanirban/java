7a1553fa8693df42803cbddfb67ce81edb23a1bd
Anirban Dutta <duttaanirbanad@gmail.com>
2024-10-20 18:49:07 +0530

    findEle

--- recursion/arrays/findElements.java
+++ recursion/arrays/findElements.java
@@ -1,11 +1,13 @@
 package arrays;
-
+import java.util.ArrayList;
 public class findElements {
     public static void main(String[] args) {
         int[] arr = {2, 3, 1, 4, 5, 8, 4, 12};
         System.out.println(findele(arr, 4, 0));
         System.out.println(find(arr, 4, 0));
         System.out.println(findFromLast(arr, 4, arr.length - 1));
+        findAllIndex(arr, 4, 0);
+        System.out.println(list);
     }
 
     static boolean findele(int[] arr, int target, int index) {
@@ -37,5 +39,16 @@ public class findElements {
             else {
             return findFromLast(arr, target, index-1);
             }
-        }
+    }
+
+        static ArrayList<Integer> list = new ArrayList<>();
+        static void findAllIndex(int[] arr, int target, int index) {
+            if (index == arr.length) {
+                return;
+            }
+            if (arr[index] == target) {
+                list.add(index);
+            }
+            findAllIndex(arr, target, index+1);
+    }
 }
