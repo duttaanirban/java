42b100baa90cce33f9690ea32ef43d36375ed5db
Anirban Dutta <duttaanirbanad@gmail.com>
2024-10-20 17:53:40 +0530

    countSteps

---
+++ recursion/countSteps.java
@@ -0,0 +1,20 @@
+public class countSteps {
+    public static void main(String[] args) {
+        System.out.println(numberOfSteps(50068890));
+    }
+
+    static int numberOfSteps(int num) {
+        return helper(num, 0);
+    }    
+
+    static int helper(int num, int steps) {
+        if (num == 0) {
+            return steps;
+        }
+
+        if (num % 2 == 0) {
+            return helper(num/2,steps+1);
+        }
+        return helper(num-1, steps+1);
+    }
+}
