38e91aaedfec9e24cceee645d5346e0b51f70880
Anirban Dutta <duttaanirbanad@gmail.com>
2024-10-20 18:23:39 +0530

    updated

---
+++ recursion/arrays/sorted.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class sorted {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 0, 5, 6, 7, 8, 9};
+        System.out.println(Sorted(arr, 0));
+    }
+
+    static boolean Sorted(int[] arr, int index) {
+        if (index == arr.length - 1) {
+            return true;
+        }
+
+        return arr[index] < arr[index + 1] && Sorted(arr, index + 1);
+    }
+}
