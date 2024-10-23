93043f533b8e8bd3ff5b0059049e9a86d07420c4
Anirban Dutta <duttaanirbanad@gmail.com>
2024-10-23 20:07:39 +0530

    new

--- Binarysearch/BinarySearch.java
+++ Binarysearch/BinarySearch.java
@@ -1,6 +1,5 @@
 import java .util.Scanner;
 
-
 public class BinarySearch {
     public static void main(String[] args) {
         // Sample sorted array
--- recursion/triangle.java
+++ recursion/triangle.java
@@ -1,20 +1,34 @@
 public class triangle {
     public static void main(String[] args) {
-        pattern(4, 0);
+        pattern2(4, 0);
     }
 
     static void pattern(int row, int col) {
         if (row == 0) {
             return;
         }
-
         if (col < row) {
-            System.out.print("* ");
+            System.out.print("*");
             pattern(row, col + 1);
         }
         else {
             System.out.println();
             pattern(row - 1, 0);
+        }
+    }
+
+    static void pattern2(int row, int col) {
+        if (row == 0) {
+            return;
+        }
+
+        if (col < row) {
+            pattern2(row, col + 1);
+            System.out.print("*");
+        }
+        else {
+            pattern2(row - 1, 0);
+            System.out.println();
         }
     }
 }
