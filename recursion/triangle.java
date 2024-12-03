f5ad9435ab6d9a64fa4a9778921d5e78f6ca3cdf
Anirban Dutta <duttaanirbanad@gmail.com>
2024-12-03 18:12:07 +0530

    new

--- recursion/triangle.java
+++ recursion/triangle.java
@@ -1,6 +1,10 @@
+import java.util.Arrays;
+
 public class triangle {
     public static void main(String[] args) {
-        pattern(4, 0);
+        int[] arr = {4, 3, 2, 1};
+        Selection(arr, arr.length, 0, 0);
+        System.out.println(Arrays.toString(arr));
     }
 
     static void pattern(int row, int col) {
@@ -8,7 +12,7 @@ public class triangle {
             return;
         }
         if (col < row) {
-            System.out.print("*");
+            System.out.print("* ");
             pattern(row, col + 1);
         }
         else {
@@ -24,11 +28,31 @@ public class triangle {
 
         if (col < row) {
             pattern2(row, col + 1);
-            System.out.print("*");
+            System.out.print("* ");
         }
         else {
             pattern2(row - 1, 0);
             System.out.println();
+        }
+    }
+
+    static void Selection(int[] arr, int row, int col, int max) {
+        if (row == 0) {
+            return;
+        }
+        if (col < row) {
+            if (arr[col] > arr[max]) {
+                Selection(arr, row, col + 1, col);
+            }
+            else {
+                Selection(arr, row, col + 1, max);
+            }
+        }
+        else {
+            int temp = arr[max];
+            arr[max] = arr[row - 1];
+            arr[row - 1] = temp;
+            Selection(arr, row - 1, 0, 0);
         }
     }
 
