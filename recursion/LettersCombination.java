b8d321540df170b51d1287e74424368e5e199808
duttaanirban <duttaanirbanad@gmail.com>
2025-04-14 14:33:06 +0530

    letterComb

---
+++ recursion/LettersCombination.java
@@ -0,0 +1,41 @@
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class LettersCombination {
+    public static void main(String[] args) {
+        //pad("", "12");
+        System.out.println(padRet("", "12"));
+
+    }
+
+    static void pad(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+        int digit = up.charAt(0) - '0'; 
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            pad(p + ch, up.substring(1));
+        }
+    }
+
+    static ArrayList<String> padRet(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        int digit = up.charAt(0) - '0'; 
+        ArrayList<String> ans = new ArrayList<>();
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            ans.addAll(padRet(p + ch, up.substring(1)));
+        }
+        return ans;
+    }
+
+     
+}
