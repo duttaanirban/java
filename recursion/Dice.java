a8188826489d4be471f77d56f5d82887e05d799f
duttaanirban <duttaanirbanad@gmail.com>
2025-04-15 01:11:09 +0530

    diceThrow

---
+++ recursion/Dice.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Dice {
+    public static void main(String[] args) {
+        //dice("", 4);
+        System.out.println(diceRet("", 4));
+    }
+
+    static void dice(String p, int target) {
+        if (target == 0) {
+            System.out.println(p);
+            return;
+        }
+        for (int i = 1; i <= 6 && i <= target; i++) {
+            dice(p + i, target - i);
+        }
+    }
+
+    static ArrayList<String> diceRet(String p, int target) {
+        if (target == 0) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> ans = new ArrayList<>();
+        for (int i = 1; i <= 6 && i <= target; i++) {
+            ans.addAll(diceRet(p + i, target - i));
+        }
+        return ans;
+    }
+}
