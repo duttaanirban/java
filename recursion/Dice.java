230ced6e8829dc1fbb76376cc938e2433a68a09d
duttaanirban <duttaanirbanad@gmail.com>
2025-04-17 21:12:35 +0530

    simple

--- recursion/Dice.java
+++ recursion/Dice.java
@@ -6,15 +6,15 @@ public class Dice {
         System.out.println(diceRet("", 4));
     }
 
-    static void dice(String p, int target) {
-        if (target == 0) {
-            System.out.println(p);
-            return;
-        }
-        for (int i = 1; i <= 6 && i <= target; i++) {
-            dice(p + i, target - i);
-        }
-    }
+    // static void dice(String p, int target) {
+    //     if (target == 0) {
+    //         System.out.println(p);
+    //         return;
+    //     }
+    //     for (int i = 1; i <= 6 && i <= target; i++) {
+    //         dice(p + i, target - i);
+    //     }
+    // }
 
     static ArrayList<String> diceRet(String p, int target) {
         if (target == 0) {
