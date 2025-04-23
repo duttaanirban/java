205e754734c54e9543c263707f94b810704f9fcf
duttaanirban <duttaanirbanad@gmail.com>
2025-04-23 12:12:24 +0530

    test

---
+++ Strings/toLowerCase.java
@@ -0,0 +1,7 @@
+public class  toLowerCase {
+    public static void main(String[] args) {
+        String str = "HELLO WORLD";
+        String lowerStr = str.toLowerCase();
+        System.out.println(lowerStr); // Output: hello world
+    }
+}
