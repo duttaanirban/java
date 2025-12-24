7c6b850732a769fa8e90e7a637993542af2d95c1
duttaanirban <duttaanirbanad@gmail.com>
2025-12-24 19:42:37 +0530

    nqueen

---
+++ Nqueens/NQueens.java
@@ -0,0 +1,64 @@
+public class NQueens {
+
+    public static void main(String[] args) {
+        int n = 6;
+        boolean[][] board = new boolean[n][n];
+        System.out.println(queens(board, 0));
+    }
+
+    static int queens(boolean[][] board, int row) {
+        if (row == board.length) {
+            display(board);
+            return 1;
+        }
+        int count = 0;
+        // placing the queen and checking for every column
+        for (int col = 0; col < board.length; col++) {
+            // place the queen if safe
+            if (isSafe(board, row, col)) {
+                board[row][col] = true;
+                count += queens(board, row + 1);
+                board[row][col] = false; // backtrack
+            }
+        }
+        return count;
+    }
+
+    private static boolean isSafe(boolean[][] board, int row, int col) {
+        // check vertical row
+        for (int i = 0; i < row; i++) {
+            if (board[i][col]) {
+                return false;
+            }
+        }
+        // check left diagonal
+        int maxLeft = Math.min(row, col);
+        for (int i = 1; i <= maxLeft; i++) {
+            if (board[row - i][col - i]) {
+                return false;
+            }
+        }
+        // check right diagonal
+        int maxRight = Math.min(row, board.length - col - 1);
+        for (int i = 1; i <= maxRight; i++) {
+            if (board[row - i][col + i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void display(boolean[][] board) {
+    for (boolean[] row : board) {
+        for (boolean element : row) {
+            if (element) {
+                System.out.print("Q ");
+            } else {
+                System.out.print("X ");
+            }
+        }
+        System.out.println(); // new line after each row
+    }
+    System.out.println(); // space between solutions
+}
+}
